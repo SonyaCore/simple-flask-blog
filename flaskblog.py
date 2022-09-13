4c198b150d0eda2590fb6b01bfc257eba2d4da8b
SonyaCore <sonyadream@protonmail.com>
2022-09-13 13:46:34 +0430

    added more context for layout.html

--- flaskblog.py
+++ flaskblog.py
@@ -13,14 +13,24 @@ main_post = [
         'content': '''Flask is a micro web framework written in Python. It is classified as a microframework because it does not require particular tools or libraries.It has no database abstraction layer, form validation, or any other components where pre-existing third-party libraries provide common functions. However, Flask supports extensions that can add application features as if they were implemented in Flask itself. Extensions exist for object-relational mappers, form validation, upload handling, various open authentication technologies and several common framework related tools.'''
     },
 ]
+
+# Additional Navbar Information
 @app.context_processor
 def layout():
     url = {
-        'github': 'https://github.com/SoniaCore',
-        'telegram': 'https://telegram.me/ReiLibre',
-
+        'github':      'https://github.com/SoniaCore',
+        'telegram':    'https://telegram.me/ReiLibre',
+        'info':        'Still in Development @SoniaCore',
     }
     return dict(url=url)
+
+# Blog Name
+@app.context_processor
+def name():
+    info = {
+        'servername':   'Sonia Blog'
+    }
+    return dict(info=info)
 
 @app.route('/')
 @app.route('/home')
