118eb734a8041af52a2643118070fa2d444145a2
Sonya Fatholahi <sonyadream@protonmail.com>
2022-09-14 09:12:15 +0430

    added database tables

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import email
 from flask import Flask , render_template , url_for, flash , redirect
 from forms import RegistrationForm , LoginForm
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from config import APIKEY , SQLDB
 
 # App Name
@@ -17,7 +17,23 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
+    password = db.Column(db.String(60),nullable=False)
+    posts = db.relationship('Post',backref='author',lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.password}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    data_posted = db.Column(db.DateTime(120), nullable=False,default=datetime.utcnow)
+    content = db.Column(db.Text,nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.data_posted}')"
 
 posts = [
     {
