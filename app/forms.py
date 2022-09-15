a3d40cad81e2d9fda0d2d6d83bd83793981c7124
Sonya Fatholahi <sonyadream@protonmail.com>
2022-09-16 02:45:36 +0430

    added profile update and profile picture update

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,23 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField , FileAllowed 
+from flask_login import current_user
 from wtforms import StringField , PasswordField , SubmitField , BooleanField
 from wtforms.validators import DataRequired,Length , Email , EqualTo , ValidationError
 from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                            validators=[DataRequired(),Length(min=2,max=10)])
+                            validators=[DataRequired(),
+                            Length(min=2,max=10)])
     email = StringField('Email',
-                            validators=[DataRequired(),Email()])
+                            validators=[DataRequired(),
+                            Email()])
     password = PasswordField('Password',
                             validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                            validators=[DataRequired(),EqualTo('password')])
+                            validators=[DataRequired(),
+                            EqualTo('password')])
+    
     submit = SubmitField('Sign Up')
 
     def validate_username(self,username):
@@ -26,9 +32,35 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-                            validators=[DataRequired(),Email()])
+                            validators=[DataRequired(),
+                            Email()])
     password = PasswordField('Password',
                             validators=[DataRequired()])
 
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                            validators=[DataRequired(),
+                            Length(min=2,max=10)])
+    email = StringField('Email',
+                            validators=[DataRequired(),
+                            Email()])
+
+    picture = FileField('Update Porfile Picture',
+                            validators=[FileAllowed(['jpg','png','jpeg'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('ََThe username already exist!')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('The email already exist!')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template , url_for, flash , redirect , request
 from app import app , db , bcrypt
-from app.forms import RegistrationForm , LoginForm
+from app.forms import RegistrationForm , LoginForm , UpdateAccountForm
 from app.models import User,Post
 from flask_login import login_user , current_user , logout_user , login_required
+from PIL import Image
+
+import secrets
+import os
+
 
 posts = [
     {
@@ -34,7 +39,8 @@ def name():
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html',posts=posts)
+    return render_template('home.html',
+    posts=posts)
 
 @app.route('/about')
 def about():
@@ -47,12 +53,23 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') # Hash password
-        user = User(username=form.username.data,email=form.email.data,password=hashed_password) # Add User
-        db.session.add(user) # Commit to DB
+
+        # Add User
+        user = User(username=form.username.data,
+        email=form.email.data,
+        password=hashed_password)
+
+        # Commit to DB
+        db.session.add(user)
         db.session.commit()
-        flash(f'{form.username.data} has been created !', category='success')
+
+        flash(f'{form.username.data} has been created !',
+        category='success')
+
         return redirect(url_for('login'))
-    return render_template('register.html',title='Register', form = form)
+    return render_template('register.html',
+    title='Register',
+    form = form)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -68,14 +85,60 @@ def login():
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Failed Check your information!', category='danger')
-    return render_template('login.html',title='Login', form = form)
+    return render_template('login.html',
+    title='Login',
+    form = form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+
+def save_picture(form_picture):
+    # Generate Hex Token for Profile Picture
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = UpdateAccountForm().username.data + '_' + random_hex + f_ext
+
+    # Join Picture
+    picture_path = os.path.join(app.root_path, 'static/profile', picture_fn)
+
+    # Avatar Picure Resolution
+    output_size = (125,125)
+
+    # Final Picture Resolution
+    picture = Image.open(form_picture)
+    picture.thumbnail(output_size)
+
+    # Save Picture to File System
+    picture.save(picture_path)
+
+    return picture_fn
+
+
+@app.route('/account',methods=['GET','POST'])
 @login_required
 def account():
-    return render_template('account.html',title='My Profile')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            old_pic = current_user.image_file
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+            if old_pic != 'default.jpg':
+                os.remove(os.path.join(app.root_path, 'static/profile', old_pic))
+    
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated!' , category='success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    imagefile = url_for('static', filename =f"profile/{current_user.image_file}")
+    return render_template('account.html',
+    title = 'My Profile',
+    profile = imagefile ,
+    form = form)
