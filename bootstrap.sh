77071d7e344a6b5c5d62cc2a568c741fe8f9f198
SonyaCore <sonyacore@protonmail.com>
2022-10-11 13:52:33 +0330

    Added Bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export SECRET_KEY='Your Secret Key'
+export SQLALCHEMY_DATABASE_URI='DataBase URI Path'
+
+export MAIL_SERVER='SMTP Server URL'
+export SMTP_PORT='SMTP PORT'
+
+export MAIL_USERNAME='SMTP USERNAME'
+export MAIL_PASSWORD='SMTP PASSWORD'
+
+python3 run.py
