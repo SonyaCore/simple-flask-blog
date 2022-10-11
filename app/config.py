70444a6f956a4eb291800d1be41ab2f46cc58d3f
SonyaCore <sonyacore@protonmail.com>
2022-10-11 13:53:57 +0330

    updated config env

--- app/config.py
+++ app/config.py
@@ -1,2 +1,14 @@
-APIKEY = '225ad17a7eb2bd354c3dd9efb10a0ab524940ec74b28f24d'
-SQLDB = 'sqlite:///site.db'
+# App Config
+import os
+class Config: 
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # Mail
+    MAIL_SERVER = os.environ.get('SMTP')
+    MAIL_PORT= os.environ.get('SMTP_PORT')
+    MAIL_USERNAME = os.environ.get('EMAIL_USER') 
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USE_TLS = True
