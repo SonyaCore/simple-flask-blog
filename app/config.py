6bbfe59fbd10e2e3d9a3a94e86fc2198b881d557
SonyaCore <sonyacore@protonmail.com>
2022-10-12 16:26:36 +0330

    added Constant validator for user & pass check

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 # App Config
-import os
+import os , re
 class Config: 
 
     SECRET_KEY = os.environ.get('SECRET_KEY') 
@@ -12,3 +12,8 @@ class Config:
     MAIL_USERNAME = os.environ.get('EMAIL_USER') 
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
     MAIL_USE_TLS = True
+
+# Constant Validation
+VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._'
+PASSWORD_REGEX = re.compile(r'\A(?=\S*?\d)(?=\S*?[A-Z])(?=\S*?[a-z])\S{6,}\Z')
+USERNAME_REGEX = re.compile(r'\A[\w\-\.]{3,}\Z')
